d466adc5e142f6a3a795fae0161ef32c29fa86c6
Shandavine-500 <chemutaibrenda794@gmail.com>
2024-05-14 20:33:56 +0300

    first project

---
+++ script.js
@@ -0,0 +1,174 @@
+function firstname(firstname) {
+    console.log("hey" + firstname)
+}
+
+function lastname(lastname) {
+    console.log("hello" + lastname)
+}
+var myName;
+myName = "Cherono"
+//function call
+firstname("Brenda")
+lastname("Chemutai")
+
+console.log(myName)
+var a = "hello world"
+
+var test = 12;
+function exam() {
+    console.log(test);
+
+}
+exam()
+function addTwoNumbers(params) {
+    var b = 10;
+    var c = 12;
+    var d = b + c;
+
+    console.log(a)
+    console.log(b)
+    console.log(c)
+    console.log(d)
+}
+addTwoNumbers()
+var test = 30;
+function exam() {
+    console.log(test);
+}
+exam()
+function sum() {
+    var a = 40;
+    var b = 30;
+    var c = a + b;
+    console.log(a)
+    console.log(b)
+    console.log(c)
+}
+sum()
+var num = 0;
+if (num > 0) {
+    console.log("the number is positive.");
+}
+else if (num < 0) {
+    console.log("the number negative.");
+} else {
+    console.log("the number is zero.");
+}
+var grade = 80;
+
+function checkGrade() {
+    if (grade > 80) {
+        console.log("A")
+    }
+    else if (grade >= 70 && grade <= 80) {
+        console.log("B")
+    }
+    else if (grade >= 60 && grade <= 70) {
+        console.log("C")
+    }
+    else {
+        console.log("D")
+    }
+}
+checkGrade()
+var grade = 100;
+function marks() {
+    if (grade > 100) {
+        console.log("A")
+    }
+
+    else if (grade >= 80 && grade <= 100) {
+        console.log("B")
+
+    }
+    else { console.log("C") }
+}
+marks()
+var marks = 60;
+function unit() {
+    if (marks > 60) {
+        console.log("B")
+    }
+    else if (marks >= 40 && marks <= 60) {
+        console.log("C")
+    }
+    else { console.log("D") }
+}
+unit()
+// for(let i=0;i>10;$$<=20;i++){
+//     console.log("i");
+// }
+
+
+for (let i = 0; i < 20; i++) {
+    console.log(i)
+}
+
+for (let k = 20; k > 0; k--) {
+    console.log(k)
+}
+for (var n = 4; n > 0; n--) {
+    console.log(n)
+}
+for (var m = 0; m <= 7; m++) {
+    console.log(m)
+}
+for (s = 5; s > 0; s--) {
+    console.log(s)
+}
+// addingtwonumbers()
+var num1 = 50;
+var num2 = 60;
+var sum = num1 + num2
+function exam() {
+    console.log(50);
+    console.log(60);
+    console.log(50 + 60);
+}
+exam()
+var results = 30;
+function output() {
+    if (results > 30) {
+        console.log("C")
+    }
+    else if (results > 20 && results <= 3) {
+        console.log("D")
+    }
+    else { console.log("E") }
+}
+output()
+
+let numbers = [1, 2, 3, 4, 5]
+console.log(numbers[4])
+let fruits = ["apples", "oranges", "mangoes", "bananas", "pineapples"]
+console.log(fruits[4])
+
+fruits.forEach((fruit) => {
+    console.log(fruit)
+})
+numbers.forEach((num) => {
+    console.log(num)
+}
+
+)
+console.log(numbers.length)
+let units = ["maths", "coding", "commskill"]
+console.log(units[1])
+units.forEach((subject) => {
+    console.log(subject)
+}
+
+)
+// console.log (units.length)
+
+for (var index = 0; index < units.length; index++) {
+    console.log(units[index]);
+}
+for (var index = units.length - 1; index >= 0; index--) {
+    console.log(units[index]);
+}
+for(var index=0;index<fruits.length;index++){
+console.log(fruits[index]);}
+
+
+
